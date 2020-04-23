cb95e9c8a37f5a76b405cdd3adc6c2040fe93908
JoshuaHall <joshuahallmail@gmail.com>
2020-04-23 16:27:28 -0500

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,10 @@
+module Main exposing (main)
+
+import Html exposing (div, text)
+import Html.Attributes exposing (id)
+
+
+main =
+    div
+        [ id "quote-box" ]
+        [ text "this is the div#quote-box" ]
